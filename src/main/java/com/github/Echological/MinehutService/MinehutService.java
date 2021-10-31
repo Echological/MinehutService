6df623fa6350c822a482478aea528a0fb06e5426
Seth Weintraub <setheweintraub@me.com>
2021-10-31 12:19:28 -0700

    init

---
+++ src/main/java/com/github/Echological/MinehutService/MinehutService.java
@@ -0,0 +1,25 @@
+package com.github.Echological.MinehutService;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
+
+import java.util.function.Consumer;
+
+public record MinehutService() {
+
+    static String id = System.getenv("SERVER_ID");
+
+    public static void server(final Consumer<JSONObject> consumer){
+        HttpResponse<JsonNode> res = Unirest.get("https://api.minehut.com/server/"+id)
+                .asJson();
+
+        JsonNode node = res.getBody();
+
+        JSONObject obj = node.getObject();
+
+        consumer.accept(obj);
+    }
+
+}
