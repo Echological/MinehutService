eb5d7d953479f00487620e7480e1ee4c9673c7d7
Seth Weintraub <setheweintraub@me.com>
2021-10-31 13:27:05 -0700

    works?

--- src/main/java/com/github/Echological/MinehutService/MinehutService.java
+++ src/main/java/com/github/Echological/MinehutService/MinehutService.java
@@ -3,6 +3,7 @@ package com.github.Echological.MinehutService;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
 import kong.unirest.json.JSONObject;
 
 import java.util.function.Consumer;
@@ -11,15 +12,37 @@ public class MinehutService {
 
     static String id = System.getenv("SERVER_ID");
 
-    public static void server(final Consumer<JSONObject> consumer){
+    static JSONObject data() {
         HttpResponse<JsonNode> res = Unirest.get("https://api.minehut.com/server/"+id)
                 .asJson();
 
         JsonNode node = res.getBody();
 
-        JSONObject obj = node.getObject();
+        return node.getObject().getJSONObject("server");
+    }
 
-        consumer.accept(obj);
+    public static void data(final Consumer<JSONObject> consumer){
+        consumer.accept(data());
+    }
+
+    public static void name(final Consumer<String> consumer){
+        JSONObject data = data();
+        consumer.accept(data.getString("name"));
+    }
+
+    public static void online(final Consumer<Boolean> consumer){
+        JSONObject data = data();
+        consumer.accept(data.getBoolean("online"));
+    }
+
+    public static void players(final Consumer<JSONArray> consumer){
+        JSONObject data = data();
+        consumer.accept(data.getJSONArray("players"));
+    }
+
+    public static void motd(final Consumer<String> consumer){
+        JSONObject data = data();
+        consumer.accept(data.getString("motd"));
     }
 
 }
